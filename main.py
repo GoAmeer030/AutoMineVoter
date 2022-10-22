be934cbceb6c84083396f403bba52a5b915dddbe
GoAmeer030 <ameer03062003@gmail.com>
2022-10-22 04:39:57 +0100

    option for proxy change has added, few bug fixes and code improvements

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ final_proxies = []
 
 
 class new_proxy:
+    """
+    Some Times only this will work because I don't know how to make it work
+    """
+
     def __len__(self) -> int:
         return len(final_proxies)
 
@@ -36,8 +40,8 @@ class new_proxy:
                 proxy_data[headers[i]] = tds[i].text.strip()
             proxies.append(proxy_data)
 
-        for i in proxies:
-            final_proxies.append(f'{i["IP Address"]}:{i["Port"]}')
+        final_proxies.append(f'{i["IP Address"]}:{i["Port"]}' for i in proxies if
+                             i["Code"] in ["US", "IN", "SG", "DE", "CA"] and i["Google"] in ["yes", "no"])
 
     @staticmethod
     def proxy(driver) -> str:
@@ -48,76 +52,67 @@ class new_proxy:
 
 
 class Oplegends:
-    def __init__(self, option, driver, user=".GoAmeer030") -> None:
+    def __init__(self, option, user, is_proxy_change) -> None:
         self.user = user
         self.option = option
-        self.driver = driver
-        self.minecraft_server_list(self.option, self.driver)
+        self.driver = webdriver.Chrome(
+            service=Service(r'.\chromedriver.exe'), options=self.option)
+        self.is_proxy_change = is_proxy_change
+        self.minecraft_server_list()
+        self.driver_close()
 
-    def minecraft_server_list(self, option, driver):
-        driver = main.change_proxy(option=option, driver=driver)
+    def minecraft_server_list(self):
+        driver = main.change_proxy(option=self.option) if self.is_proxy_change == "-y" else self.driver
         driver.get("https://minecraft-server-list.com/server/443038/vote/")
         # print("MSL opened")
-        text = driver.find_element(By.NAME, 'ignn')
+        text = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.NAME, 'ignn')))
         text.send_keys(self.user)
         time.sleep(3)
-        button = WebDriverWait(driver, 5).until(
-            EC.visibility_of_element_located((By.XPATH, '//*[@id="voteform"]/input[5]')))
+        button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="voteform"]/input[5]')))
         button.click()
         # print("Clicked")
-        # noinspection PyBroadException
-        try:
-            result = WebDriverWait(driver, 20).until(
-                EC.visibility_of_element_located((By.XPATH, '//*[@id="voteerror"]')))
-        except Exception:
-            button.click()
-            result = WebDriverWait(driver, 20).until(
-                EC.visibility_of_element_located((By.XPATH, '//*[@id="voteerror"]')))
-        while ((result.text in (
-                "Please Wait....", "Error with the Anti Spam check - Please try again. (Click F5 to reload the page).",
+        time.sleep(3)
+        result = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="voteerror"]')))
+        print("Oplegends : Minecraft Server List:", result.text)
+        while ((result.text in ("Error with the Anti Spam check - Please try again. (Click F5 to reload the page).",
                 "")) or ("Username already voted today!" in result.text)):
-            if result.text == "Please Wait....":
-                time.sleep(1)
-                result = WebDriverWait(driver, 10).until(
-                    EC.visibility_of_element_located((By.XPATH, '//*[@id="voteform"]/input[5]')))
-            elif "Username already voted today!" in result.text:
+            if "Username already voted today!" in result.text:
                 # print("elif")
                 break
             else:
                 # print("recursion")
-                self.minecraft_server_list(option=option, driver=driver)
-        else:
-            print(result.text)
-            driver.close()
+                self.minecraft_server_list()
+
+    def driver_close(self) -> None:
+        self.driver.close()
 
 
 class main:
 
-    def __init__(self, user='.GoAmeer030') -> None:
+    def __init__(self, user='.GoAmeer030', is_proxy_change='-n') -> None:
         option = webdriver.ChromeOptions()
-        # self.option.add_argument("headless")
+        option.add_argument("headless")
         option.add_experimental_option('useAutomationExtension', False)
-        driver = webdriver.Chrome(service=Service(r'./chromedriver.exe'), options=option)
 
-        Oplegends(option=option, driver=driver, user=user)
+        Oplegends(option=option, user=user, is_proxy_change=is_proxy_change)
 
     @staticmethod
-    def change_proxy(option, driver):
+    def change_proxy(option):
+        driver = webdriver.Chrome(service=Service(r'.\chromedriver.exe'), options=option)
         proxy = new_proxy.proxy(driver=driver)
         # print(proxy)
-        # webdriver.DesiredCapabilities.CHROME['proxy'] = {
-        #     "httpProxy": proxy,
-        #     "ftpProxy": proxy,
-        #     "sslProxy": proxy,
-        #     "proxyType": "MANUAL",
-        # }
-        # webdriver.DesiredCapabilities.CHROME['acceptSslCerts'] = True
         option.add_argument(f'--proxy-server={proxy}')
+        webdriver.DesiredCapabilities.CHROME['acceptSslCerts'] = True
         driver = webdriver.Chrome(service=Service(r'.\chromedriver.exe'), options=option)
         return driver
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
-    # main(".dhiangarvijay27")
-    # main()
+    try:
+        main(sys.argv[1], sys.argv[2])
+    except IndexError:
+        main()
+        # main(".dhiangarvijay27")
